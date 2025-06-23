fc2493b152a79f90d7cced618c9cd5058ba3ee4f
Labasse <labasse@gmail.com>
2025-06-23 17:35:26 +0000

    Associer un ViewSet (fin)

---
+++ recipes/urls.py
@@ -0,0 +1,8 @@
+from rest_framework import routers
+
+from .views import CategoryViewSet
+
+router = routers.DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+
+urlpatterns = router.urls
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Category
+from .serializers import CategorySerializer
+from rest_framework import viewsets
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing category instances.
+    """
+    queryset = Category.objects.all().order_by('order')
+    serializer_class = CategorySerializer
