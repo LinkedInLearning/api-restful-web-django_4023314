69b30dff180a0d31d1c8820fafbea76cf9184e8c
Labasse <labasse@gmail.com>
2025-06-24 17:41:03 +0000

    Varier le tri (fin)

--- gourmand/settings.py
+++ gourmand/settings.py
@@ -137,6 +137,11 @@ REST_FRAMEWORK = {
     # "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PagePagination",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
+    ],
 }
 
 # Default primary key field type
